663de977ceba606d9143cdba8e2c4ca21652c288
ZhaoDong <7942160+zhao_dong1181803971@user.noreply.gitee.com>
2021-11-24 21:02:13 +0800

    easyExcel

---
+++ src/main/java/com/dzu/easyexcel/easy/DemoData.java
@@ -0,0 +1,33 @@
+package com.dzu.easyexcel.easy;
+
+import com.alibaba.excel.annotation.ExcelIgnore;
+import com.alibaba.excel.annotation.ExcelProperty;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+/**
+ * @author by ZhaoDong
+ * @Classname DemoData
+ * @Description TODO
+ * @Date 2021/11/24 20:33
+ */
+@Getter
+@Setter
+@EqualsAndHashCode
+public class DemoData {
+
+    @ExcelProperty("字符串标题")
+    private String string;
+    @ExcelProperty("日期标题")
+    private Date date;
+    @ExcelProperty("数字标题")
+    private Double doubleData;
+    /**
+     * 忽略这个字段
+     */
+    @ExcelIgnore
+    private String ignore;
+}
---
+++ src/main/java/com/dzu/easyexcel/easy/EasyExcelTest.java
@@ -0,0 +1,78 @@
+package com.dzu.easyexcel.easy;
+
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.write.metadata.WriteSheet;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author by ZhaoDong
+ * @Classname Test
+ * @Description yuque.com/easyexcel/doc/easyexcel
+ * @Date 2021/11/24 20:35
+ */
+public class EasyExcelTest {
+    private static final String PATH = "E:\\JAVA\\easy-excel\\excel\\";
+
+    public static void main(String[] args) {
+        String fileName = PATH + "simpleWrite" + System.currentTimeMillis() + ".xlsx";
+        // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
+        // 如果这里想使用03 则 传入excelType参数即可
+        EasyExcel.write(fileName, DemoData.class).sheet("模板").doWrite(data());
+    }
+
+    private static List<DemoData> data() {
+        List<DemoData> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            DemoData data = new DemoData();
+            data.setString("字符串" + i);
+            data.setDate(new Date());
+            data.setDoubleData(0.56);
+            list.add(data);
+        }
+        return list;
+        // 根据list 写入 excel
+    }
+
+    /**
+     * 最简单的写
+     * <p>
+     * 1. 创建excel对应的实体对象 参照{@link DemoData}
+     * <p>
+     * 2. 直接写即可
+     */
+    public void simpleWrite() {
+        // 写法1 JDK8+
+        // since: 3.0.0-beta1
+        String fileName = PATH + "simpleWrite" + System.currentTimeMillis() + ".xlsx";
+        // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
+        // 如果这里想使用03 则 传入excelType参数即可
+        EasyExcel.write(fileName, DemoData.class)
+                .sheet("模板")
+                .doWrite(data());
+
+        // 写法2
+        fileName = PATH + "simpleWrite" + System.currentTimeMillis() + ".xlsx";
+        // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
+        // 如果这里想使用03 则 传入excelType参数即可
+        EasyExcel.write(fileName, DemoData.class).sheet("模板").doWrite(data());
+
+        // 写法3
+        fileName = PATH + "simpleWrite" + System.currentTimeMillis() + ".xlsx";
+        // 这里 需要指定写用哪个class去写
+        ExcelWriter excelWriter = null;
+        try {
+            excelWriter = EasyExcel.write(fileName, DemoData.class).build();
+            WriteSheet writeSheet = EasyExcel.writerSheet("模板").build();
+            excelWriter.write(data(), writeSheet);
+        } finally {
+            // 千万别忘记finish 会帮忙关闭流
+            if (excelWriter != null) {
+                excelWriter.finish();
+            }
+        }
+    }
+}
